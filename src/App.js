32236e1cb6c63f83cd8d26f2dc90a3a055a8a2b6
Duke John Tiwari <duke.john90@gmail.com>
2023-01-05 21:48:25 +0530

    Pushing codes

---
+++ src/App.js
@@ -0,0 +1,94 @@
+
+import { useState } from "react";
+import "./App.css";
+function App() {
+  const [students, setStudents] = useState([]);
+  const [checkin, setCheckin] = useState(false);
+  const [checkout, setCheckout] = useState(false);
+  const [present, setPresent] = useState(false);
+  const handlesubmit = (e) => {
+    e.preventDefault();
+    const name = e.target[0].value;
+    const roll = e.target[1].value;
+    const classs = e.target[2].value;
+    const section = e.target[3].value;
+    const student = {
+      name,
+      roll,
+      classs,
+      section,
+    };
+    setStudents([...students, student]);
+    setCheckin(false);
+  };
+  const handlecheckout = (e) => {
+    e.preventDefault();
+    const roll = e.target[0].value;
+    const student = students.find((student) => student.roll === roll);
+    if (!student) {
+      alert("Student not checked in");
+      return;
+    }
+    const newstudents = students.filter((student) => student.roll !== roll);
+    setStudents(newstudents);
+    setCheckout(false);
+  };
+
+  return (
+    <div class="text-bg-info">
+    <div>
+      <h1 class="studentHeader bg-primary">Student Attendance</h1>
+    </div>
+      <div>
+        <button  type="button" class="checkinButton btn btn-success btn-lg" onClick={() => setCheckin(!checkin)}>Check In</button>
+        <button type="button" class="checkoutButton btn btn-danger btn-lg" onClick={() => setCheckout(!checkout)}>Check Out</button>
+        {checkin && (
+          <form onSubmit={(e) => handlesubmit(e)}>
+            <input type="text" placeholder="Enter Name" />
+            <input type="text" placeholder="Enter Roll no." />
+            <input type="text" placeholder="Enter Class" />
+            <input type="text" placeholder="Enter Section" />
+            <button type="submit">Submit</button>
+          </form>
+        )}
+        <br />
+        {checkout && (
+          <form onSubmit={(e) => handlecheckout(e)}>
+            <input type="text" placeholder="Enter Roll no." />
+            <button type="submit">Submit</button>
+          </form>
+        )}
+        <br />
+        <button  type="button" class="presentButton btn btn-dark"
+          onClick={() => {
+            students.length > 0
+              ? setPresent(!present)
+              : alert("No students present");
+          }}
+        >
+          Present
+        </button>
+        {present && (
+          <table >
+            <tr class="headingRow" >
+              <th>Name</th>
+              <th>Roll</th>
+              <th>Class</th>
+              <th>Section</th>
+            </tr>
+            {students.map((student) => (
+              <tr>
+                <td>{student.name}</td>
+                <td>{student.roll}</td>
+                <td>{student.classs}</td>
+                <td>{student.section}</td>
+              </tr>
+            ))}
+          </table>
+        )}
+      </div>
+    </div>
+  );
+}
+
+export default App;
